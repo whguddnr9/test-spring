d2cd8e70d7c559e2dfbbb347979034274b312e1e
whguddnr9 <whguddnr9@naver.com>
2021-05-20 01:38:49 +0900

    mac환경 오류 해결 test중

--- src/test/java/com/hjo/example/springboot/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/hjo/example/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -42,5 +42,6 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+        //오류 수정중
     }
 }
